f950a41661ffec376a691dbdace8e90cac8c32ca
Varun Jatav <mohit.mohit979@gmail.com>
2024-10-28 20:44:16 +0530

    navigation done working on caraousel

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MainCarousel from "./components/HomeCarousel";
+import Navigation from "./components/Navigation";
+
+function App() {
+
+  return (
+    <>
+      <Navigation/>
+      <MainCarousel/>
+    </>
+  );
+}
+
+export default App;
