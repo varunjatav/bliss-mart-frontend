70baa8f4d185156ccbea96fae954234a81915a60
Varun Jatav <mohit.mohit979@gmail.com>
2024-11-06 16:34:58 +0530

    added scroll to top.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
 
   return (
     <>
      <Navigation/>
+     <ScrollToTop/>
      <Outlet/>
      <Footer/>
     </>
---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+   // Extracts pathname property(key) from an object
+   const { pathname } = useLocation();
+
+   // Automatically scrolls to top whenever pathname changes
+  
+    useEffect(() => {
+    if(!pathname.includes("/syllabus") && !pathname.includes("/instructor")){
+      window.scrollTo(0, 0);
+    }
+    
+   
+    }, [pathname]);
+   
+  return null;
+}
+
+export default ScrollToTop;
