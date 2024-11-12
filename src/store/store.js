91c9d9f9184615e5df56af947e36bf8b08d9a43c
Varun Jatav <mohit.mohit979@gmail.com>
2024-11-12 17:47:30 +0530

    hjkui

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
 import authSlice from "./authSlice";
 import cartSlice from "./cartSlice";
-import productSlice from "./productSlice";
+// import productSlice from "./productSlice";
 
 import { configureStore } from "@reduxjs/toolkit";
 
 const blissMartStore = configureStore({
     name:"blissMart",
     reducer: {
-        products: productSlice.reducer,
+        // products: productSlice.reducer,
         cart: cartSlice.reducer,
         auth: authSlice.reducer
     }
